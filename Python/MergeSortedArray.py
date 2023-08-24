56de0b006bc1d0d7fbc4f3cc248ebc031a956924
Ahmed Ehab Saleh <a.saleh.eja@ncgr.gov.sa>
2023-08-24 17:21:32 +0200

    Solved 88. Merge Sorted Array

--- Python/MergeSortedArray.py
+++ Python/MergeSortedArray.py
@@ -1,49 +1,30 @@
 from typing import List
 
 
+# Solved using leetcode solution : https://leetcode.com/problems/merge-sorted-array/solutions/3037315/fully-explained-java-code-with-approach-in-o-m-n-time/
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
 
-        if n == 0:
-            return nums1
-        if m == 0:
-            i = 0
-            while i < len(nums2):
-                nums1.insert(i, nums2[i])
-                nums1.pop()
-                i += 1
-            return nums1
-
-        i, j = 0, 0  # i for nums1, j for num2
-        while i < m and j < n:
-            if nums1[i] >= nums2[j]:
-                nums1.insert(i, nums2[j])
-                i += 2
-                j += 1
-            elif nums1[i] < nums2[j]:
-                i += 1
-        if i > m:
-            i = m
-            while i < len(nums1) and j < n:
-                nums1.insert(i, nums2[j])
-                i += 1
-                j += 1
-
-        for x in range(0, n):
-            nums1.pop()
-        return nums1
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
 
 
 obj = Solution()
 
-# print(obj.merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
+print(obj.merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
 # print(obj.merge([1], 1, [], 0))
 # print(obj.merge([0], 0, [1], 1))
 # print(obj.merge([1, 0], 1, [2], 1))
 # print(obj.merge([1, 2, 3, 0, 0, 0], 3, [4, 5, 6], 3))
-print(obj.merge([-1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0], 5, [-1, -1, 0, 0, 1, 2], 6))
+# print(obj.merge([-1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0], 5, [-1, -1, 0, 0, 1, 2], 6))
 # print(obj.merge([2, 0], 1, [1], 1))
 # print(obj.merge([-1, 0, 0, 3, 3, 3, 0, 0, 0], 6, [1, 2, 2], 3))
